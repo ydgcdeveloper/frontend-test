c312494130ae88ea82ab3606ba0e62904f631009
ydgcdeveloper <ydgcdeveloper@gmail.com>
2024-04-18 13:16:47 -0500

    set main routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component'),
+  },
+  {
+    path: 'product-details/:id',
+    loadComponent: () => import('./pages/product-details/product-details.component'),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/home/home.component'),
+  },
+];

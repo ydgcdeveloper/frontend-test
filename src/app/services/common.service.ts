4dd3bfd303a78f7541ffd557bb815ae3b28aeba0
ydgcdeveloper <ydgcdeveloper@gmail.com>
2024-04-19 09:11:07 -0500

    add loadProducts method to service

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -1,13 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommonService {
+  private PRODUCT_FILE_DATA_PATH = 'assets/data/DatosScraping.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.loadProducts();
+  }
 
-  getProduct(id: string){
-    
+  loadProducts() {
+    return this.http.get<any[]>(this.PRODUCT_FILE_DATA_PATH).pipe(
+      map((products: { [key: string]: any }) => {
+        const productArray = Object.keys(products).map(key => products[key]);
+        return productArray.map((product: any, index: number) => ({ ...product, id: index }));
+      })
+    );
+  }
+
+  readJsonFile(filePath: string): Observable<any> {
+    return this.http.get<any>(filePath);
   }
 }

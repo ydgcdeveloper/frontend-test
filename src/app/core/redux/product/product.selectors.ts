09a8157ca30028374869a70b12d761a6c3c1ce37
ydgcdeveloper <ydgcdeveloper@gmail.com>
2024-04-19 14:44:42 -0500

    add selector by category

--- src/app/core/redux/product/product.selectors.ts
+++ src/app/core/redux/product/product.selectors.ts
@@ -1,15 +1,24 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromProducts from './product.reducers';
 
-export const selectProductsState = createFeatureSelector<fromProducts.ProductsState>(
-  fromProducts.productsFeatureKey
-);
+export const selectProductsState =
+  createFeatureSelector<fromProducts.ProductsState>(
+    fromProducts.productsFeatureKey
+  );
 
 export const selectAllProducts =
   fromProducts.adapter.getSelectors(selectProductsState).selectAll;
 
 export const selectProductById = (productId: number) =>
   createSelector(selectProductsState, (state) => state.entities[productId]);
+
+export const selectProductsByCategory = (categoryName: string) =>
+  createSelector(selectProductsState, (state) => {
+    const products = Object.values(state.entities);
+    return products.filter(
+      (product) => product?.Category?.replaceAll(' ', '') === categoryName
+    );
+  });
 
 export const selectLoadingProducts = createSelector(
   selectProductsState,

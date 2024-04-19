8d13fe6ed9ce9e77a0db271c6c74004a686a1631
ydgcdeveloper <ydgcdeveloper@gmail.com>
2024-04-19 14:46:15 -0500

    create products detail component

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -2,20 +2,20 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonService } from '../../services/common.service';
 import { FooterComponent } from '../home/components/footer/footer.component';
 import { Store } from '@ngrx/store';
-import { loadProducts, selectProductById } from '../../core/redux/product';
+import { selectProductById } from '../../core/redux/product';
 import { ProductState } from '../../core/redux';
 import { IProduct } from '../../core/interfaces/product.interface';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ReviewsComponent } from './components/reviews/reviews.component';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [FooterComponent, ReviewsComponent],
+  imports: [RouterModule, FooterComponent, ReviewsComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css',
 })
-export default class ProductDetailsComponent implements OnInit {
+export default class ProductDetailsComponent{
   product: IProduct | undefined;
 
   @Input()
@@ -28,15 +28,10 @@ export default class ProductDetailsComponent implements OnInit {
     });
   }
 
-  productId: number | undefined;
-
   constructor(
-    private commonService: CommonService,
     private router: Router,
     private store: Store<ProductState>
   ) {}
-
-  ngOnInit(): void {}
 
   goToOriginal(){
     this.router.navigateByUrl('home');

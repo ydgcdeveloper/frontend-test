800af703e1595ed793c84254543920e4398c2c1e
ydgcdeveloper <ydgcdeveloper@gmail.com>
2024-04-19 14:45:26 -0500

    load products on load app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { loadProducts } from './core/redux/product';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend-test';
+
+  constructor(private store: Store) {
+    this.store.dispatch(loadProducts());
+  }
 }
